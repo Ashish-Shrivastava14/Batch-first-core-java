61c6e91fdbe432e1c5d1f5cf5d9db566517bbc31
''Ashish <elx.ashish@gmail.com>
2024-09-15 01:38:50 +0530

    First Commit

---
+++ SeleniumWebDriver/src/test/java/Day1/SecondTestCase.java
@@ -0,0 +1,43 @@
+package Day1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/*1) Launch browser (chrome)
+2) Open URL  https://demo.nopcommerce.com/
+3) Validate title should be   "nopCommerce demo store"
+4) close page
+*/
+
+
+
+public class SecondTestCase {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver=new ChromeDriver(); //For chrome browser
+		
+		//2) Open URL  https://demo.opencart.com/
+				
+	 driver.get("https://demo.nopcommerce.com/");
+	 
+	 // 3) Validate title should be   "Your Store"
+	 
+	 String act_title=driver.getTitle();
+	 
+	 if(act_title.equals("nopCommerce demo store"))
+		{
+			System.out.println("Test Passed");
+		}
+		else
+		{
+			System.out.println("Test Failed");
+		}
+	 //4) close browser
+	 //driver.close();
+	 //driver.quit();
+	 
+
+	}
+
+}
