1896de55f78cb4776c3bebde7e0127caffa344b5
''Ashish <elx.ashish@gmail.com>
2024-09-16 22:56:51 +0530

    Four Commit

--- SeleniumWebDriver/src/test/java/Day2/LocatorDemo.java
+++ SeleniumWebDriver/src/test/java/Day2/LocatorDemo.java
@@ -1,5 +1,7 @@
 package Day2;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -26,10 +28,23 @@ public class LocatorDemo {
 		//boolean status=driver.findElement(By.id("logo")).isDisplayed();
 				//System.out.println("Display status:"+ status);
 				
-				//linkText & partialLinkText   --  only for link
+		//linkText & partialLinkText   --  only for link
 				
 				//driver.findElement(By.linkText("Tablets")).click();   //linkText  //preferable
-				driver.findElement(By.partialLinkText("Tab")).click();  //partialLinkText
+				//driver.findElement(By.partialLinkText("Tab")).click();  //partialLinkText
+		
+		//classname
+				//List<WebElement> headerLinks=driver.findElements(By.className("list-inline-item"));
+				//System.out.println(headerLinks.size()); // 7
+				
+		//tagname
+				List<WebElement> links=driver.findElements(By.tagName("a"));
+				System.out.println(links.size());
+				
+
+				
+		
+
 
 
 
