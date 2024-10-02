a8e0d8e9e1ce049a36ffae3215c9617fccbd2dae
''Ashish <elx.ashish@gmail.com>
2024-10-02 22:22:30 +0530

    first commit

---
+++ Batch-first-core-java/src/day1/SecondJavaProgram.java
@@ -0,0 +1,17 @@
+package day1;
+
+public class SecondJavaProgram {
+
+	public static void main(String[] args) {
+		
+System.out.println("My name is Ashish Shrivastava");
+		
+		//System.out.println("Welcome To Java");
+		
+		/*
+		 * System.out.print("Welcome"); System.out.println("To java");
+		 */
+  
+	}
+
+}
